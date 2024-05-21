76aa8d4e9c8ef8a9fb587bf56fefc11ab067f841
chogamy <gamy0315@daum.net>
2024-05-21 07:57:39 +0000

    wip

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+import argparse
+
+from srcs.parser.model import parse_model
+
+
 if __name__ == "__main__":
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--mode", required=True, default=None, type=str, choices=["infer", "train"]
+    )
+
+    args = parser.parse_args()
+
+    # model and tokenizer
+    model, tokenizer = parse_model(args)
+
+    # trainer
+    if args.mode == "train":
+        pass
+
+    if args.mode == "infer":
+        pass
---
+++ srcs/parser/model.py
@@ -0,0 +1,3 @@
+def parse_model(args):
+
+    pass
