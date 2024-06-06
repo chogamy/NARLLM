0bc750d96fd3f00054dff86afde1bc1d84ce1d20
chogamy <gamy0315@daum.net>
2024-06-06 12:55:45 +0900

    fix: pad to max length

--- srcs/data/preprocess.py
+++ srcs/data/preprocess.py
@@ -13,7 +13,7 @@ def ConvAI2(batch, tokenizer, prompt):
     tgt_batch = batch["target"]
 
     inputs = [
-        f"{prompt} {yours} {partners} {dialogue} length: {len(tokenizer(tgt, padding=False, truncation=False)['input_ids'])}"
+        f"{prompt} {yours} {partners} {dialogue} length: "
         for yours, partners, dialogue, tgt in zip(
             yours_batch, partners_batch, dialogue_batch, tgt_batch
         )
@@ -25,16 +25,20 @@ def ConvAI2(batch, tokenizer, prompt):
     """
     length_batch = tokenizer(
         [f"{tokenizer.bos_token}{inp}" for inp in inputs],
-        padding=True,
+        padding="max_length",
         truncation=True,
-        max_length=tokenizer.model_max_length,
+        max_length=int(tokenizer.model_max_length / 2),
     )
-    length_batch["target"] = tokenizer(
-        [f"{inp}{tokenizer.eos_token}" for inp in inputs],
-        padding=True,
-        truncation=True,
-        max_length=tokenizer.model_max_length,
-    )["input_ids"]
+
+    length_batch["target"] = [
+        len(tokenizer(tgt, padding=False, truncation=False)["input_ids"])
+        for tgt in tgt_batch
+    ]
+
+    inputs = [
+        f"{inp}{len(tokenizer(tgt, padding=False, truncation=False)['input_ids'])} "
+        for inp, tgt in zip(inputs, tgt_batch)
+    ]
 
     """
     input:  [bos]	[p]	        [length]	[mask]	[eos]
@@ -45,9 +49,9 @@ def ConvAI2(batch, tokenizer, prompt):
             f"{tokenizer.bos_token}{inp}{tokenizer.bos_token * len(tokenizer(tgt, padding=False, truncation=False)['input_ids'])}{tokenizer.eos_token}"
             for inp, tgt in zip(inputs, tgt_batch)
         ],
-        padding=True,
+        padding="max_length",
         truncation=True,
-        max_length=tokenizer.model_max_length,
+        max_length=int(tokenizer.model_max_length / 2),
     )
 
     target_batch["target"] = tokenizer(
@@ -55,9 +59,9 @@ def ConvAI2(batch, tokenizer, prompt):
             f"{inp}{tokenizer.eos_token}{tgt}{tokenizer.eos_token}"
             for inp, tgt in zip(inputs, tgt_batch)
         ],
-        padding=True,
+        padding="max_length",
         truncation=True,
-        max_length=tokenizer.model_max_length,
+        max_length=int(tokenizer.model_max_length / 2),
     )["input_ids"]
 
     length_input = {f"length_{k}": v for k, v in length_batch.items()}
